0c484c4e8cdf5b23dbe1197098026e8736ccb46d
isoreus <TKullan@gmail.com>
2023-12-18 16:26:55 +0300

    Section design

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function index() {
+
+        return view('chat.index');
+
+    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,10 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// VUE PART
+
+import { createApp } from 'vue';
+import  App  from './App.vue';
+
+createApp(App).mount('#chatApp');
